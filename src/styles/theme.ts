442c85a256742a7d4beb83278a52c311a3754a81
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-06 01:26:49 +0900

    chore: emotion 색상 테마, 테마 타입 선언 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+const theme = {
+  COLORS: {
+    LABEL_PRIMARY: '#111111',
+    LABEL_SECONDARY: '#767680',
+    LABEL_TERTIARY: '#c4c4c6',
+    GRAY: {
+      1: 'rgb(142, 142, 147)',
+      2: 'rgb(174, 174, 178)',
+      3: 'rgb(199, 199, 204)',
+      4: 'rgb(209, 209, 214)',
+      5: 'rgb(229, 229, 234)',
+      6: 'rgb(242, 242, 247)',
+    },
+    BACKGROUND: '#f7f7fb',
+    LANDING_BACKGROUND: {
+      1: '#aeccf0',
+      2: '#a2aced',
+    },
+  },
+} as const;
+
+export default theme;
+
+export type Theme = typeof theme;
