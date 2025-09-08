29a5df6577b5cc4727cbd974b89c86e9c636cfbb
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-08 15:10:57 +0900

    chore: 개발 서버에 https 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
+import mkcert from 'vite-plugin-mkcert';
 import path from 'path';
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+    },
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
@@ -13,6 +21,7 @@ export default defineConfig({
       },
     }),
     svgrPlugin(),
+    mkcert(),
   ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
