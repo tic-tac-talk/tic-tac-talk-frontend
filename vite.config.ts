06656a00bc0a8e7a3443e5610beda46d13884fa0
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-09 15:22:38 +0900

    chore: 테스트 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest/config" />
+import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgrPlugin from 'vite-plugin-svgr';
 import mkcert from 'vite-plugin-mkcert';
 import path from 'path';
 import fs from 'fs';
+import { type InlineConfig } from 'vitest/node';
 
-// https://vite.dev/config/
+interface VitestExtendedConfig extends UserConfig {
+  test: InlineConfig;
+}
+
 export default defineConfig({
+  test: {
+    include: ['src/**/*.test.{ts,tsx}'],
+    globals: true,
+    environment: 'jsdom',
+  },
   server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
@@ -26,4 +36,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-});
+} as VitestExtendedConfig);
