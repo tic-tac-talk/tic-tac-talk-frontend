ab049f160e2f19d288c4bf97aa317fcf205e1bbd
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-14 14:24:32 +0900

    chore(vite): 개발 서버 프록시 및 환경 변수 설정
    - API 요청을 백엔드로 프록시
    - 환경 변수 로드 기능 추가
    - 개발 환경에서만 HTTPS 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,59 @@
 /// <reference types="vitest/config" />
-import { defineConfig, type UserConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import mkcert from 'vite-plugin-mkcert';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv, type UserConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
+import svgrPlugin from 'vite-plugin-svgr';
 import { type InlineConfig } from 'vitest/node';
 
 interface VitestExtendedConfig extends UserConfig {
   test: InlineConfig;
 }
 
-export default defineConfig({
-  test: {
-    include: ['src/**/*.test.{ts,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    test: {
+      include: ['src/**/*.test.{ts,tsx}'],
+      globals: true,
+      environment: 'jsdom',
     },
-  },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
+    define: {
+      global: 'globalThis',
+    },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          secure: true,
+          ws: true,
+        },
       },
-    }),
-    svgrPlugin(),
-    mkcert(),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg'],
-  },
-} as VitestExtendedConfig);
+      ...(command === 'serve' && {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+        },
+      }),
+    },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      svgrPlugin(),
+      mkcert(),
+    ],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    optimizeDeps: {
+      exclude: ['@ffmpeg/ffmpeg'],
+    },
+  } as VitestExtendedConfig;
+});
