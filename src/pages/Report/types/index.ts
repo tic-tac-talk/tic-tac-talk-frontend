3c4e838d921ef4303ec3b226c2eb6f71119e3560
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-28 16:25:34 +0900

    feat(Report): 레포트 카드 데이터 타입 정의

---
+++ src/pages/Report/types/index.ts
@@ -0,0 +1,30 @@
+import type {
+  SummaryContent,
+  AnalysisContent,
+  BehaviorContent,
+  MistakeContent,
+  CoachingContent,
+  RatioContent,
+  ReportCard,
+} from '@/types/api';
+
+export interface ParticipantNames {
+  A: string;
+  B: string;
+}
+
+export interface BaseCardProps {
+  title: string;
+  hasBeenActivated?: boolean;
+  isActive?: boolean;
+}
+
+export type {
+  ReportCard,
+  SummaryContent,
+  AnalysisContent,
+  BehaviorContent,
+  MistakeContent as MistakesContent,
+  CoachingContent,
+  RatioContent,
+};
