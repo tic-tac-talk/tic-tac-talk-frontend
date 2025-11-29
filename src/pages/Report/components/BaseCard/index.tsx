7ef75baede3bf8b55170c7d8d4f86d3de360067e
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-29 19:47:29 +0900

    feat(Report): BaseCard 컴포넌트 구현

---
+++ src/pages/Report/components/BaseCard/index.tsx
@@ -0,0 +1,40 @@
+import type { ReactNode } from 'react';
+import FloatingImage from '@/components/FloatingImage';
+import * as S from '../BaseCard.styles';
+
+interface BaseCardProps {
+  title: string;
+  imageSrc: string;
+  hasBeenActivated: boolean;
+  isActive: boolean;
+  children: ReactNode;
+}
+
+const BaseCard = ({
+  title,
+  imageSrc,
+  hasBeenActivated,
+  isActive,
+  children,
+}: BaseCardProps) => {
+  return (
+    <S.Container isActive={isActive}>
+      <S.ContentWrapper>
+        <S.FloatingImageWrapper
+          hasBeenActivated={hasBeenActivated}
+          isActive={isActive}
+        >
+          <FloatingImage src={imageSrc} />
+        </S.FloatingImageWrapper>
+        <S.FixedContent hasBeenActivated={hasBeenActivated} isActive={isActive}>
+          <S.Title>{title}</S.Title>
+        </S.FixedContent>
+        <S.CardContent hasBeenActivated={hasBeenActivated} isActive={isActive}>
+          {children}
+        </S.CardContent>
+      </S.ContentWrapper>
+    </S.Container>
+  );
+};
+
+export default BaseCard;
