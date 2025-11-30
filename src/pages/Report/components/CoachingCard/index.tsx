c0904245e1528210868e793c9d508c7e9b301eb7
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-30 15:15:18 +0900

    feat(Report): CoachingCard 컴포넌트 구현

---
+++ src/pages/Report/components/CoachingCard/index.tsx
@@ -0,0 +1,50 @@
+import SuggestionImage from '@/assets/images/suggestion.png';
+import BaseCard from '@/pages/Report/components/BaseCard';
+import type {
+  BaseCardProps,
+  CoachingContent,
+  ParticipantNames,
+} from '@/pages/Report/types';
+import * as S from '../BaseCard.styles';
+
+interface CoachingCardProps extends BaseCardProps {
+  content: CoachingContent;
+  participantNames: ParticipantNames;
+}
+
+const CoachingCard = ({
+  title,
+  content,
+  participantNames,
+  hasBeenActivated = false,
+  isActive = true,
+}: CoachingCardProps) => {
+  return (
+    <BaseCard
+      title={title}
+      imageSrc={SuggestionImage}
+      hasBeenActivated={hasBeenActivated}
+      isActive={isActive}
+    >
+      <S.ParticipantSection>
+        <S.ParticipantName>{participantNames.A} 님에게</S.ParticipantName>
+        <S.AdviceList>
+          {content.adviceA.map((advice) => (
+            <S.AdviceItem key={advice}>{advice}</S.AdviceItem>
+          ))}
+        </S.AdviceList>
+      </S.ParticipantSection>
+
+      <S.ParticipantSection>
+        <S.ParticipantName>{participantNames.B} 님에게</S.ParticipantName>
+        <S.AdviceList>
+          {content.adviceB.map((advice) => (
+            <S.AdviceItem key={advice}>{advice}</S.AdviceItem>
+          ))}
+        </S.AdviceList>
+      </S.ParticipantSection>
+    </BaseCard>
+  );
+};
+
+export default CoachingCard;
