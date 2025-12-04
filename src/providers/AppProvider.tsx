e0214023da5d66aec65d77d56ef9319c8f4ce857
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-12-05 01:49:18 +0900

    feat(providers): 전역 프로바이더 통합

---
+++ src/providers/AppProvider.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
+import UserProvider from '@/components/UserProvider';
+import { queryClient } from '@/config/queryClient';
+import GlobalStyle from '@/styles/GlobalStyle';
+import theme from '@/styles/theme';
+
+interface AppProviderProps {
+  children: React.ReactNode;
+}
+
+const AppProvider = ({ children }: AppProviderProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <UserProvider>
+          <Router>{children}</Router>
+        </UserProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default AppProvider;
