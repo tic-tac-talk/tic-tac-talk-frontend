0bda0e51d210899efc631177208a1f0c86e4cbb4
itwillbeoptimal <itwillbeoptimal@gmail.com>
2026-02-18 15:51:19 +0900

    feat(apiClient): axios 응답 처리 시 BigInt 지원을 위해 json-bigint 적용

--- src/apis/core/apiClient.ts
+++ src/apis/core/apiClient.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import JSONbig from 'json-bigint';
 import setupInterceptors from '@/apis/core/setupInterceptors';
+
+const JSONbigParser = JSONbig({
+  storeAsString: true,
+});
 
 const apiClient = axios.create({
   baseURL: '/api/v1',
@@ -11,8 +16,7 @@ const apiClient = axios.create({
     (data) => {
       if (typeof data === 'string') {
         try {
-          const processedData = data.replace(/:(\d{16,})([,\]}])/g, ':"$1"$2');
-          return JSON.parse(processedData);
+          return JSONbigParser.parse(data);
         } catch {
           return data;
         }
